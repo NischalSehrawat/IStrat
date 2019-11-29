0592edac9fe8332f89d6557109694433bcffcd11
Nischal Sehrawat <33701903+NischalSehrawat@users.noreply.github.com>
2019-11-29 09:31:02 +0100

    Removed redundant functions, added data logging function
    Removed redundant functions, added data logging function

--- S&P/models.py
+++ S&P/models.py
@@ -11,24 +11,32 @@ import matplotlib.pyplot as plt
 
 class Investor:
     
-    data = pd.read_excel("blog.xlsx")
+    data_snp = pd.read_excel("blog.xlsx")
+    total_points = len(data_snp) # Total Data points 
         
     def __init__(self):
         
         self.stocks_owned = 0 # Total stocks owned by an investor
-        self.asset_value = 0 # Total value of the investments (= remaining cash + stocks_owned * price)
         self.available_cash = 0 # Cash remaining after purchasing
         self.total_investment = 0 # Total money invested till date
+        self.data = Investor.data_snp # Make a copy of the S&P data
         
-    def DepositMoney(self, amount):
-        # This function is used to update the money deposited to the account
         
-        self.available_cash+=amount
+    def GetAssetValue(self, stock_price):
+        # This function is used to update total assets
+        asset_value = self.available_cash + stock_price * self.stocks_owned
+        return asset_value
     
-    def UpdateTotalInvestment(self, amount, idx):
-        # This function updtes total investments made to an account
-        self.total_investment+=amount
-        Investor.data.loc[idx, "Amount_Invested"] = self.total_investment
+    def LogData(self, idx, stock_price, stocks_acquired):
+        # This function is used to log data to dataframe on any given day = idx
+        
+        # Add data to dataframe
+        self.data.loc[idx, "Asset_Value"] = self.GetAssetValue(stock_price)               
+        self.data.loc[idx, "Stocks_Owned"] = self.stocks_owned
+        self.data.loc[idx, "Stocks_Acquired"] = stocks_acquired            
+        self.data.loc[idx, "Available_Cash"] = self.available_cash 
+        self.data.loc[idx, "Total_Investment"] = self.total_investment
+
 
 
     def Compute_S1(self, amount=200):
@@ -38,61 +46,59 @@ class Investor:
         Amount is the value invested monthly, default value being 200
         """
 
-        
-        data = Investor.data
-        
-        for i in range(len(data)):
+        for i in range(Investor.total_points):
             # Step 1. Add money to account
-            self.DepositMoney(amount) 
-            # Step 2. Update total investment
-            self.UpdateTotalInvestment(amount, i)
-            # Get the stock price
-            price = data.loc[i, "Price_SnP"]
+            # Update total_investment and available_cash
+            self.total_investment+=amount
+            self.available_cash+=amount
+
+            # Step 2. Get the stock price
+            stock_price = self.data.loc[i, "Price_SnP"]
             
             """
             If the stock price is more than the remaining cash, 
-            then we cannot buy any stock this month. So add the amount to 
-            the remaining cash.
+            then we cannot buy any stock this month. 
             """            
-            if self.available_cash < price:
-                self.available_cash+=amount
-                self.asset_value = self.available_cash + price * self.stocks_owned
+            if self.available_cash < stock_price:
+                '''
+                Since we didn't buy new stocks therefore 
+                new stocks acquired = 0, 
+                and total stocks owned are same as previous month
+                '''
+                stocks_acquired = 0
                 
-                # Add data to the dataframe
-                data.loc[i, "Remaining_Cash"] = self.available_cash
-                data.loc[i, "Asset_Value"] = self.asset_value
-                # Since we didn't buy new stocks or stocks acquired = 0, they are same as previous month
-                data.loc[i, "Stocks_Acquired"] = 0
-                if i == 0:                    
-                    data.loc[i, "Stocks_Owned"] = 0
-                else:                    
-                    data.loc[i, "Stocks_Owned"] = data.loc[i-1, "Stocks_Owned"]                
+                # Log data to the dataframe                
+                self.LogData(i, stock_price, stocks_acquired)
+                    
+               
             else:                
                 """
-                1. Buy stocks. Stocks can only be bought in whole numbers
-                2. Add remaining money to the remining cash
+                1. Buy stocks. 
+                    Note: Stocks can only be bought in whole numbers
+                2. Update available cash to the amount left after buying stocks
                 """
-                stocks_acquired = self.available_cash // price
+                # Buy stocks in whole number
+                stocks_acquired = self.available_cash // stock_price
+                # Update stocks owned
                 self.stocks_owned+=stocks_acquired
-                self.available_cash = self.available_cash % price                
-                self.asset_value = self.available_cash + price * self.stocks_owned
+                # Update available cash to the amount remaining after buying stocks
+                self.available_cash = self.available_cash % stock_price
+
+                # Log data to dataframe
+                self.LogData(i, stock_price, stocks_acquired)
+
                 
-                # Add data to dataframe
-                data.loc[i, "Asset_Value"] = self.asset_value               
-                data.loc[i, "Stocks_Owned"] = self.stocks_owned
-                data.loc[i, "Stocks_Acquired"] = stocks_acquired            
-                data.loc[i, "Available_Cash"] = self.available_cash
-                
-        return data
+        return self.data
 
 p = Investor().Compute_S1()    
 
+plt.close("all")
 plt.subplot(2,2,1)
 plt.plot(p["Price_SnP"], '--r', label = "S&P");plt.legend()
 plt.ylabel("S&P Index", fontsize = 14); plt.yticks(fontsize = 14)            
 plt.subplot(2,2,2)
 plt.plot(p["Asset_Value"], '--g', label = "Asset Value"); plt.legend()      
-plt.plot(p["Amount_Invested"], '--b', label = "Amount_Invested")
+plt.plot(p["Total_Investment"], '--b', label = "Total_Investment")
 plt.ylabel("Amount in USD", fontsize = 14); plt.yticks(fontsize = 14)            
 
 plt.legend()            
